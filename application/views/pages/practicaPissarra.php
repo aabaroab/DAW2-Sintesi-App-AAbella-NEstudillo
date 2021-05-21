57eea65a873fa40040d414a84ee0c61dd0acbd96
aabaroab <72941918+aabaroab@users.noreply.github.com>
2021-05-21 21:15:42 +0200

    Update practicaPissarra.php

--- application/views/pages/practicaPissarra.php
+++ application/views/pages/practicaPissarra.php
@@ -77,6 +77,8 @@
             <p class="btn btn-primary" type="button" onclick="changeTypeBorderCircle()">B.Circle</p>
             <p class="btn btn-primary" type="button" onclick="changeTypeFullRect()">R.Rectangle</p>
             <p class="btn btn-primary" type="button" onclick="changeTypeBorderRect()">B.Rectangle</p>
+	    	<br/>
+		<input type="text" id="textCanva"/>
             <p class="btn btn-primary" type="button" onclick="changeTypeText()">Text</p>
             <p class="btn btn-primary" type="button" onclick="changeTypeClearRect()">Borrar area</p>
             <p class="btn btn-primary" type="button" onclick="cleanCanva()">Borrar tot</p>
